cde298c123f032e7eadd93e837573353398e5945
David Haak <david.haak@chghealthcare.com>
2022-12-05 22:31:33 -0700

    stub day 5

---
+++ src/2022/05.test.js
@@ -0,0 +1,15 @@
+const solution = require('./05')
+const primer = `
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`
+
+test('Day 5', () => {
+  expect(solution(primer)).toEqual(['CMZ'])
+})

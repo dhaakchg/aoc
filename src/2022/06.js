0653049b20bd3ce36b6c52c0d35f361b8052284a
David Haak <david.haak@chghealthcare.com>
2022-12-06 17:22:23 -0700

    cleanup 6

--- src/2022/06.js
+++ src/2022/06.js
@@ -1,15 +1,14 @@
-module.exports = (input, step) => {
+const findMarker = (input, step) => {
   const msgBuffer = input.trim()
   let marker = 0
-  // console.log('Input length:', input.length, 'Stop:', input.length - step)
   for(let i = 0; i < input.length - step; i++) {
     marker = i + step
     let msgSlice = new Set(msgBuffer.slice(i, marker))
-    let allUnique = msgSlice.size === step
-    // console.log('i:', i, 'Slice:', msgSlice, 'Set size:', msgSlice.size, 'Marker:', marker, 'All unique?:', allUnique)
-    if(allUnique) {
-      break
-    }
+    if(msgSlice.size === step) break
   }
   return marker
+}
+
+module.exports = (input) => {
+  return [findMarker(input, 4), findMarker(input, 14)]
 }
--- src/2022/06.test.js
+++ src/2022/06.test.js
@@ -1,21 +1,11 @@
 const solution = require('./06')
 
 test.each([
-  ['bvwbjplbgvbhsrlpgdmjqwftvncz', 5],
-  ['nppdvjthqldpwncqszvftbrmjlhg', 6],
-  ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10],
-  ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11],
-])('Day 6: %p first marker after char %p', (msgBuffer, marker) => {
-  expect(solution(msgBuffer, 4)).toEqual(marker)
-})
-
-
-test.each([
-  ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19],
-  ['bvwbjplbgvbhsrlpgdmjqwftvncz', 23],
-  ['nppdvjthqldpwncqszvftbrmjlhg', 23],
-  ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29],
-  ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26],
-])('Day 6: %p first marker after char %p', (msgBuffer, marker) => {
-  expect(solution(msgBuffer, 14)).toEqual(marker)
+  ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', [7, 19]],
+  ['bvwbjplbgvbhsrlpgdmjqwftvncz', [5, 23]],
+  ['nppdvjthqldpwncqszvftbrmjlhg', [6, 23]],
+  ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', [10, 29]],
+  ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', [11, 26]],
+])('Day 6: %p first marker after char %p', (msgBuffer, markers) => {
+  expect(solution(msgBuffer)).toEqual(markers)
 })

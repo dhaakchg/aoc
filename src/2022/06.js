1792dab16eba4064d2e52ba5e665970acdfa6d87
David Haak <david.haak@chghealthcare.com>
2022-12-06 17:09:59 -0700

    day 6 part 2

--- src/2022/06.js
+++ src/2022/06.js
@@ -1,18 +1,15 @@
-const {range} = require('../util/helpers'
-)
-module.exports = (input) => {
+module.exports = (input, step) => {
   const msgBuffer = input.trim()
-  const step = 4
-  let part1marker = 0
-  console.log('Input length:', input.length, 'Stop:', input.length - step)
+  let marker = 0
+  // console.log('Input length:', input.length, 'Stop:', input.length - step)
   for(let i = 0; i < input.length - step; i++) {
-    part1marker = i + step
-    let msgSlice = new Set(msgBuffer.slice(i, part1marker))
+    marker = i + step
+    let msgSlice = new Set(msgBuffer.slice(i, marker))
     let allUnique = msgSlice.size === step
-    console.log('i:', i, 'Slice:', msgSlice, 'Set size:', msgSlice.size, 'Marker:', part1marker, 'All unique?:', allUnique)
+    // console.log('i:', i, 'Slice:', msgSlice, 'Set size:', msgSlice.size, 'Marker:', marker, 'All unique?:', allUnique)
     if(allUnique) {
       break
     }
   }
-  return part1marker
+  return marker
 }
--- src/2022/06.test.js
+++ src/2022/06.test.js
@@ -6,5 +6,16 @@ test.each([
   ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10],
   ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11],
 ])('Day 6: %p first marker after char %p', (msgBuffer, marker) => {
-  expect(solution(msgBuffer)).toEqual(marker)
+  expect(solution(msgBuffer, 4)).toEqual(marker)
+})
+
+
+test.each([
+  ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19],
+  ['bvwbjplbgvbhsrlpgdmjqwftvncz', 23],
+  ['nppdvjthqldpwncqszvftbrmjlhg', 23],
+  ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29],
+  ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26],
+])('Day 6: %p first marker after char %p', (msgBuffer, marker) => {
+  expect(solution(msgBuffer, 14)).toEqual(marker)
 })

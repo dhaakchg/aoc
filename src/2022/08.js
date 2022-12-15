3b700c3d0ecb9a2279f8e81d2d74b76d5aeb933d
David Haak <david.haak@chghealthcare.com>
2022-12-15 16:36:25 -0700

    8 part 1 slice print works

--- src/2022/08.js
+++ src/2022/08.js
@@ -1,1 +1,83 @@
-module.exports = input => { return [] }
+const {splitClean} = require("../util/inputUtils");
+
+class TreeGrid {
+  constructor(input) {
+    this.grid = []
+    input.forEach(line => {
+      this.grid.push(line.split(''))
+    })
+    this.rows = this.grid.length
+    this.columns = this.grid[0].length
+  }
+
+  getTreeSlices(r, c) {
+    return {
+      row: {
+        index: r,
+        slice: this.grid[r]
+      },
+      col: {
+        index: c,
+        slice: this.grid.map(gr => gr[c])
+      }
+    }
+  }
+
+  printTreeSlices(ts) {
+    let p = []
+    for(let row = 0; row < this.rows; row++ ) {
+      let rs = ''
+      for (let col = 0; col < this.columns; col++) {
+        if(row === ts.row.index || col === ts.col.index) {
+          rs += this.grid[row][col]
+        } else {
+          rs += '.'
+        }
+      }
+      p.push(rs)
+    }
+    return p.join('\n')
+  }
+
+  treeVisible(tp, treeSlice) {
+    let visible = false
+    if(tp === 0 || tp === treeSlice.length - 1) {
+      visible = true
+    } else {
+      const left = treeSlice.slice(0, tp)
+      const right = treeSlice.slice(tp + 1, treeSlice.length).reverse()
+      const treeHeight = treeSlice[tp]
+      if(left.filter(t => t < treeHeight).length === left.length && right.filter(t => t < treeHeight).length === right.length) {
+        visible = true
+      }
+    }
+    console.log(`Tree pos: ${tp}, slice: ${treeSlice} visible: ${visible}`)
+    return visible
+  }
+
+  countVisible() {
+    // let visible = (this.rows * 2) + ((this.columns - 2) * 2)
+    let visible = 0
+    for(let row = 1; row < this.rows - 1; row++ ) { // skip perimeter
+      for(let col = 1; col < this.columns - 1; col++ ) { // skip perimeter
+        const treeX = this.getTreeSlices(row, col)
+        console.log(`Checking tree [${row}][${col}]`)
+        console.log(this.printTreeSlices(treeX))
+        if(this.treeVisible(row, treeX.row.slice) || this.treeVisible(col, treeX.col.slice)) {
+          visible += 1
+        }
+      }
+    }
+    return visible
+  }
+
+  toString() {
+    this.grid.join('\n')
+  }
+}
+
+module.exports = input => {
+  const trees = new TreeGrid(splitClean(input))
+  console.log(trees)
+  return trees.countVisible()
+}
--- src/2022/08.test.js
+++ src/2022/08.test.js
@@ -1,6 +1,10 @@
 const solution = require('./08')
-const primer = ``
+const primer = `30373
+25512
+65332
+33549
+35390`
 
 test('Day 8', () => {
-  expect(solution(primer)).toEqual([])
+  expect(solution(primer)).toEqual([21, 0])
 })

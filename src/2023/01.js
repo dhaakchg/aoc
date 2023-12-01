b08d41f2b5883e7280cae32bfe0fb34c8630e50c
David Haak <david.haak@chghealthcare.com>
2023-12-01 15:46:36 -0700

    day 1 wtf

--- src/2023/01.js
+++ src/2023/01.js
@@ -1,6 +1,35 @@
 const { splitClean } = require("../util/inputUtils");
 
+const wordIntMap = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+}
+
+const parseReduce = (input, re) => {
+    return splitClean(input).map(line => {
+        const m = [...line.matchAll(re)].map(match => match[0] in wordIntMap ? wordIntMap[match[0]] : match[0])
+        const first = m[0]
+        const last = m[m.length - 1]
+        const num = parseInt(`${first}${last}`)
+        console.log(`line ${line} matched: ${m} first: ${first} last: ${last} parsed: ${num}`)
+        return num
+    }).reduce((p, c) => {
+        console.log(`accum: ${p} adding ${c}`)
+        return p + c
+    }, 0)
+}
+
 module.exports = (input) => {
-    const [part1, part2] = splitClean(input)
+    console.log('---- Part 1 ----')
+    const part1 = parseReduce(input, /\d/g)
+    console.log('---- Part 2 ----')
+    const part2 = parseReduce(input, /(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g)
     return { part1, part2 }
 }
--- src/2023/01.test.js
+++ src/2023/01.test.js
@@ -1,7 +1,18 @@
 const solution = require('./01')
-const primer = `primer
-input`
+const primer1 = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+
+const primer2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
 
 test('Day 01', () => {
-    expect(solution(primer)).toEqual({ part1: 'primer', part2: 'input' })
+    // expect(solution(primer1).part1).toEqual(142)
+    expect(solution(primer2).part2).toEqual(281)
 })

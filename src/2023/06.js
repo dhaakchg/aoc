2560a6d38d883ee629b6c2afc86ff6a71e04aeea
David Haak <david.haak@chghealthcare.com>
2023-12-06 17:27:07 -0700

    6p1

--- src/2023/06.js
+++ src/2023/06.js
@@ -1,4 +1,5 @@
 const {splitClean} = require("../util/inputUtils");
+const {range} = require('../util/helpers')
 
 const parseRaces = (input) => {
     const [ time, distance ] = splitClean(input).map(val => val.split(':')[1]
@@ -7,14 +8,28 @@ const parseRaces = (input) => {
     )
     const races = []
     for (let i = 0; i < time.length; i++) {
-        races.push({ time: time[i], dist: distance[i]})
+        races.push({ raceLength: time[i], recordDist: distance[i]})
     }
     console.log(races)
     return races
 }
 
+const waysToWin = (race) => {
+    const { raceLength, recordDist } = race
+    const buttonHolds = range(0, raceLength)
+    const winningHolds = []
+    for(let boatSpeed of buttonHolds) {
+        const timeLeft = (raceLength - boatSpeed)
+        const distance = boatSpeed * timeLeft
+        console.log(`Held button for: ${boatSpeed}ms, boat moved at ${boatSpeed} mm/ms for ${timeLeft}ms for total: ${distance}mm vs record: ${recordDist}`)
+        if(distance > recordDist) winningHolds.push(boatSpeed)
+    }
+    console.log(`Race: ${JSON.stringify(race)} had ${winningHolds.length} ways to win`)
+    return winningHolds.length
+}
 
 module.exports = (input) => {
     const races = parseRaces(input)
-    return { part1: 0, part2: 0 }
+    const part1 = races.map(race => waysToWin(race)).reduce((a, c) => a * c)
+    return { part1, part2: 0 }
 }

ac622638591418df01f8dfac2537c553640c0b9e
David Haak <david.haak@chghealthcare.com>
2024-12-03 20:56:56 -0700

    3p2

--- src/2024/03.js
+++ src/2024/03.js
@@ -1,13 +1,26 @@
-const getOperands = (input) => {
+const part1 = (input) => {
     return [...input.matchAll(/mul\((?<lhs>[0-9]{1,3}),(?<rhs>[0-9]{1,3})\)/g)]
       .map(match => match.groups)
       .map(({lhs, rhs}) => [parseInt(lhs), parseInt(rhs)])
+      .map(([lhs, rhs]) => lhs * rhs).reduce((a, c) => a + c)
 }
 
-const part1 = (input) => {
-    return getOperands(input).map(([lhs, rhs]) => lhs * rhs).reduce((a, c) => a + c)
+const part2 = (input) => {
+    let enabled = true
+    const operations = [...input.matchAll(/(mul\((?<lhs>[0-9]{1,3}),(?<rhs>[0-9]{1,3})\)|(?<on>do\(\))|(?<off>don't\(\)))/g)].map(match => match.groups)
+    let sum = 0
+    operations.forEach(({lhs, rhs, on, off}) => {
+        if(on) {
+            enabled = true
+        } else if(off) {
+            enabled = false
+        } else if(enabled && lhs && rhs) {
+            sum += parseInt(lhs) * parseInt(rhs)
+        }
+    })
+    return sum
 }
 
 module.exports = (input) => {
-    return { part1: part1(input) }
+    return { part1: part1(input), part2: part2(input) }
 }
--- src/2024/03.test.js
+++ src/2024/03.test.js
@@ -1,6 +1,8 @@
 const solution = require('./03')
-const primer = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const primer1 = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+const primer2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 
 test('Day 3', () => {
-    expect(solution(primer)).toEqual({ part1: 161, part2: 0 })
+    expect(solution(primer1)).toEqual({ part1: 161, part2: 161 })
+    expect(solution(primer2)).toEqual({ part1: 161, part2: 48 })
 })

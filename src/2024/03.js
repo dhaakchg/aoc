deecb5f4b382ed4838d2a2914a0c5e8864237332
David Haak <david.haak@chghealthcare.com>
2024-12-03 20:30:40 -0700

    3p1

--- src/2024/03.js
+++ src/2024/03.js
@@ -1,6 +1,13 @@
-const {splitClean} = require("../util/inputUtils");
+const getOperands = (input) => {
+    return [...input.matchAll(/mul\((?<lhs>[0-9]{1,3}),(?<rhs>[0-9]{1,3})\)/g)]
+      .map(match => match.groups)
+      .map(({lhs, rhs}) => [parseInt(lhs), parseInt(rhs)])
+}
+
+const part1 = (input) => {
+    return getOperands(input).map(([lhs, rhs]) => lhs * rhs).reduce((a, c) => a + c)
+}
 
 module.exports = (input) => {
-    const [part1, part2] = splitClean(input)
-    return { part1, part2 }
+    return { part1: part1(input) }
 }
--- src/2024/03.test.js
+++ src/2024/03.test.js
@@ -1,7 +1,6 @@
 const solution = require('./03')
-const primer = `primer
-input`
+const primer = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 
 test('Day 3', () => {
-    expect(solution(primer)).toEqual({ part1: 'primer', part2: 'input' })
+    expect(solution(primer)).toEqual({ part1: 161, part2: 0 })
 })

c3a0b8695b306ba80c94484d2a8fa3276c967865
David Haak <david.haak@chghealthcare.com>
2024-12-04 16:30:03 -0700

    4

--- src/2024/04.js
+++ src/2024/04.js
@@ -1,17 +1,38 @@
 const {splitClean} = require("../util/inputUtils");
 const Grid = require("../util/grid");
+const GridCoord = require("../util/gridCoord");
 const xmasRegex = /(?=(XMAS|SAMX))/g
 
 const findInArray = (list) => {
     return [...list.join('').matchAll(xmasRegex)].length
 }
 
-module.exports = (input) => {
+const part1 = (grid) => {
     let part1 = 0
-    const grid = new Grid({data: splitClean(input)})
     part1 += grid.getRows().map(row => findInArray(row)).reduce((acc, curr) => acc + curr)
     part1 += grid.getCols().map(row => findInArray(row)).reduce((acc, curr) => acc + curr)
     part1 += grid.getLeftToRightDiags().map(row => findInArray(row)).reduce((acc, curr) => acc + curr)
     part1 += grid.getRightToLeftDiags().map(row => findInArray(row)).reduce((acc, curr) => acc + curr)
-    return { part1, part2: 0 }
+    return part1
+}
+
+const isXmas = (grid) => {
+    return grid.as1dArray().join('').match(/(M.S.A.M.S)|(S.S.A.M.M)|(S.M.A.S.M)|(M.M.A.S.S)/)
+}
+
+const part2 = (grid) => {
+    let count = 0
+    for(let i = 1; i < grid.cols - 1; i++) {
+        for(let j = 1; j < grid.rows - 1; j++) {
+            if(isXmas(grid.getAdjacentGrid(new GridCoord(j, i)))) {
+                count++
+            }
+        }
+    }
+    return count
+}
+
+module.exports = (input) => {
+    const grid = new Grid({data: splitClean(input)})
+    return { part1: part1(grid), part2: part2(grid) }
 }

63346d982be5afbd582ce2634839ea3ba43277f7
David Haak <david.haak@chghealthcare.com>
2025-12-03 18:54:08 -0700

    aoc 2025 day 1 part 2

--- src/2025/01.js
+++ src/2025/01.js
@@ -1,18 +1,22 @@
 const {splitClean} = require('../util/inputUtils')
 
-const moveDial = (currentDial, direction, count) => {
+const moveDial = (currentDial, direction, count, midSpin = false) => {
   let zeroed = 0
   let newDialPos = currentDial
+  let fullSpins = Math.floor(count / 100)
+  if (midSpin) zeroed += fullSpins
   let move = count % 100
   if (direction === 'L') {
     newDialPos = currentDial - move
     if (newDialPos < 0) {
       newDialPos = 100 + newDialPos
+      if (midSpin && currentDial !== 0 && newDialPos !== 0) zeroed += 1 // crossed zero
     }
   } else if (direction === 'R') {
     newDialPos = currentDial + move
     if (newDialPos > 99) {
       newDialPos = newDialPos - 100
+      if (midSpin && currentDial !== 0 && newDialPos !== 0) zeroed += 1 // crossed zero
     }
   }
   if (newDialPos === 0) zeroed += 1
@@ -21,15 +25,23 @@ const moveDial = (currentDial, direction, count) => {
 
 module.exports = (input) => {
   let dial = 50
-  let zeroCount = 0
-  splitClean(input).forEach(line => {
+  let part1 = 0
+  const instructions = splitClean(input).map(line => {
     const { direction, count } = line.match(/(?<direction>[LR])(?<count>\d+)/).groups
-    const { newDialPos, zeroed } = moveDial(dial, direction, parseInt(count, 10))
+    return { direction, count: parseInt(count, 10) }
+  })
+  instructions.forEach(({ direction, count }) => {
+    const { newDialPos, zeroed } = moveDial(dial, direction, count)
     dial = newDialPos
-    zeroCount += zeroed
-    console.log(`Dial at: ${dial}, zeroed: ${zeroCount}`)
+    part1 += zeroed
   })
 
-
-  return { part1: zeroCount, part2: 0 }
+  dial = 50
+  let part2 = 0
+  instructions.forEach(({ direction, count }) => {
+    const { newDialPos, zeroed } = moveDial(dial, direction, count, true)
+    dial = newDialPos
+    part2 += zeroed
+  })
+  return { part1, part2 }
 }
